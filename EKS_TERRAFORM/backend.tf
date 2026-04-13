ec6b934769b29da16dc0785f91dd165ba0fb94b3
basheepv <basherpv@gmail.com>
2026-04-13 17:13:05 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "basheer-eks-bucket" # Replace with your actual S3 bucket name
+    bucket = "basheer-eks-bucket-11" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
